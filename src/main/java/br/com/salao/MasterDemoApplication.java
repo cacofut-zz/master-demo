3268077cac8a4ed2b0bfc95072b9171a762db4a1
Cristiano Carvalho Amaral <cacofut@hotmail.com>
2019-11-28 15:36:25 -0300

    Primeiro commit

---
+++ src/main/java/br/com/salao/MasterDemoApplication.java
@@ -0,0 +1,13 @@
+package br.com.salao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterDemoApplication.class, args);
+	}
+
+}
